9a1cdeddb6c5458f730e0147db74813a31fecbbe
Erika <Erika@HPBLACK>
2024-11-29 12:16:18 +0100

    Add project files.

---
+++ SharingPics/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PictureUploader
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.Button btnUploadImage;
+        private System.Windows.Forms.TextBox txtImageUrl;
+        private System.Windows.Forms.PictureBox pictureBox;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.btnUploadImage = new System.Windows.Forms.Button();
+            this.txtImageUrl = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(9, 10);
+            this.btnSelectImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(90, 24);
+            this.btnSelectImage.TabIndex = 0;
+            this.btnSelectImage.Text = "Select Image";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // btnUploadImage
+            // 
+            this.btnUploadImage.Location = new System.Drawing.Point(104, 10);
+            this.btnUploadImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUploadImage.Name = "btnUploadImage";
+            this.btnUploadImage.Size = new System.Drawing.Size(90, 24);
+            this.btnUploadImage.TabIndex = 1;
+            this.btnUploadImage.Text = "Upload Image";
+            this.btnUploadImage.UseVisualStyleBackColor = true;
+            this.btnUploadImage.Click += new System.EventHandler(this.btnUploadImage_Click);
+            // 
+            // txtImageUrl
+            // 
+            this.txtImageUrl.Location = new System.Drawing.Point(9, 39);
+            this.txtImageUrl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtImageUrl.Name = "txtImageUrl";
+            this.txtImageUrl.ReadOnly = true;
+            this.txtImageUrl.Size = new System.Drawing.Size(451, 20);
+            this.txtImageUrl.TabIndex = 2;
+            this.txtImageUrl.TextChanged += new System.EventHandler(this.txtImageUrl_TextChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(9, 62);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(450, 325);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 396);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.txtImageUrl);
+            this.Controls.Add(this.btnUploadImage);
+            this.Controls.Add(this.btnSelectImage);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Picture Uploader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ SharingPics/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace PictureUploader
+{
+    public partial class Form1 : Form
+    {
+        private string selectedImagePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    selectedImagePath = ofd.FileName;
+                    pictureBox.Image = Image.FromFile(selectedImagePath);
+                }
+            }
+        }
+
+        private void btnUploadImage_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedImagePath))
+            {
+                MessageBox.Show("Please select an image first.");
+                return;
+            }
+
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=testingappsbobteilcilla;AccountKey=cccjOI6Mtk7pJALdK8oJ/hMPTkVgqOGoWsOwSlUoxoRKiWauqhT4IjvpxEWENcT+ELPth6GcxLjf+AStlC/dvw==;EndpointSuffix=core.windows.net";
+            string containerName = "pictures";
+
+            string blobUrl = UploadFileToBlobStorage(connectionString, containerName, selectedImagePath);
+
+            if (blobUrl != null)
+            {
+                txtImageUrl.Text = blobUrl;
+                MessageBox.Show("Image uploaded successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Failed to upload image.");
+            }
+        }
+
+        private string UploadFileToBlobStorage(string connectionString, string containerName, string localFilePath)
+        {
+            try
+            {
+                BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+                BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+                containerClient.CreateIfNotExists(PublicAccessType.Blob);
+
+                string fileName = Path.GetFileName(localFilePath);
+                BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+                using (FileStream uploadFileStream = File.OpenRead(localFilePath))
+                {
+                    blobClient.Upload(uploadFileStream, true);
+                }
+
+                return blobClient.Uri.AbsoluteUri;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+                return null;
+            }
+        }
+
+        
+    }
+}
